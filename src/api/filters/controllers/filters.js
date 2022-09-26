cc1fa426a7afe1e44ef154549a10816a76abc630
feitosadavi <davifeitosa.dev@protonmail.com>
2022-09-26 03:43:05 -0300

    .

--- src/api/filters/controllers/filters.js
+++ src/api/filters/controllers/filters.js
@@ -7,7 +7,6 @@
 module.exports = {
   async find (ctx) {
     const { query, params } = ctx
-    console.log({ query, params });
 
     const data = await strapi
       .service("api::filters.filters")
