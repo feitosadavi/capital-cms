261d74ed6fedb3f3b4976906413c5cc50aa29e2f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-10-04 00:42:35 -0300

    .

--- src/api/marca/controllers/marca.js
+++ src/api/marca/controllers/marca.js
@@ -6,4 +6,38 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::marca.marca');
+const sanitizePhoto = ({ url, alternativeText }) => ({ src: url, alt: alternativeText })
+
+const sanitizeData = (data, isMultiple) => {
+  if (isMultiple) {
+    const fieldsArray = []
+    for (const field of data) {
+      const sanitizedField = sanitizeField(field)
+      fieldsArray.push({ ...sanitizedField })
+    }
+    return fieldsArray
+  }
+  const sanitizedField = sanitizeField(data)
+  return sanitizedField
+}
+
+const sanitizeField = (field) => {
+  const data = { id: field.id, ...field.attributes }
+  const sanitizedPhotos = sanitizePhoto(data.photo.data.attributes)
+  return { ...data, photo: sanitizedPhotos }
+}
+
+module.exports = createCoreController('api::marca.marca', ({ strapi }) => ({
+  async find (ctx) {
+    const { data, meta } = await super.find(ctx)
+    const sanitizedData = sanitizeData(data, true)
+    return { meta, data: sanitizedData }
+  },
+
+  async findOne (ctx) {
+    const { meta, data } = await super.findOne(ctx)
+    const sanitizedData = sanitizeData(data)
+    return { meta, data: sanitizedData }
+  }
+
+}));
--- src/api/veiculo/controllers/veiculo.js
+++ src/api/veiculo/controllers/veiculo.js
@@ -6,7 +6,8 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-const sanitizePhotos = (photos) => photos.map(({ attributes: { url, alternativeText } }) => ({ src: url, alt: alternativeText }))
+const sanitizePhoto = ({ url, alternativeText }) => ({ src: url, alt: alternativeText })
+const sanitizePhotos = (photos) => photos.map(({ attributes }) => sanitizePhoto(attributes))
 
 const sanitizeData = (data, isMultiple) => {
   if (isMultiple) {
@@ -25,23 +26,38 @@ const sanitizeVehicle = (vehicle) => {
   const sanitizedVehicle = { id: vehicle.id }
   const vehicleAttributes = vehicle.attributes
   for (const key of Object.keys(vehicleAttributes)) {
-    sanitizedVehicle[key] = (vehicleAttributes[key]?.data?.attributes?.label || vehicleAttributes[key])
+    if (key === 'marca') {
+      const { label, photo } = vehicleAttributes[key]?.data?.attributes
+      sanitizedVehicle[key] = {
+        label: label,
+        photo: sanitizePhoto(photo.data.attributes)
+      }
+    } else {
+      sanitizedVehicle[key] = (vehicleAttributes[key]?.data?.attributes?.label || vehicleAttributes[key])
+    }
   }
-  const sanitizedPhotos = sanitizePhotos(sanitizedVehicle.photos.data)
+
+  console.log(sanitizedVehicle.photos.data);
+  const sanitizedPhotos = sanitizePhotos(sanitizedVehicle?.photos.data)
   return { ...sanitizedVehicle, photos: sanitizedPhotos }
 }
 
 module.exports = createCoreController('api::veiculo.veiculo', ({ strapi }) => ({
   async find (ctx) {
     const { data, meta } = await super.find(ctx)
-    console.log({ data });
-    const sanitizedData = sanitizeData(data, true)
-    console.log({ sanitizedData });
-    return { meta, data: sanitizedData }
+    try {
+
+      const sanitizedData = sanitizeData(data, true)
+      return { meta, data: sanitizedData }
+    } catch (error) {
+      console.log(error);
+      return data
+    }
   },
 
   async findOne (ctx) {
     const { meta, data } = await super.findOne(ctx)
+    // const marca = sanitizeField(data)
     const sanitizedData = sanitizeData(data)
     return { meta, data: sanitizedData }
   }
